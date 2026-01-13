a696c6a24e9a60283993158f5a92a62b0cb745d9
joseph <mmthejhb025@student.wethinkcode.co.za>
2026-01-13 15:50:54 +0200

    one_done

---
+++ basics/calculator.py
@@ -0,0 +1,18 @@
+def main():
+    user = input("")
+    addition()
+    substraction()
+    multiplication()
+    division()
+    
+def addition(a,b):
+    return a + b
+    
+def substraction(a,b):
+    return a - b
+    
+def multiplication(a,b):
+    return a * b
+    
+def division(a,b):
+    return a/b
