3302e421ad031b000bd8a04ce0845457a94295c8
joseph <mmthejhb025@student.wethinkcode.co.za>
2026-01-14 08:10:59 +0200

    2nd_done

--- basics/calculator.py
+++ basics/calculator.py
@@ -1,18 +1,43 @@
-def main():
-    user = input("")
-    addition()
-    substraction()
-    multiplication()
-    division()
-    
-def addition(a,b):
-    return a + b
-    
-def substraction(a,b):
-    return a - b
-    
-def multiplication(a,b):
-    return a * b
-    
-def division(a,b):
-    return a/b
+
+while True:
+    def main():
+        user_operation = input("Choose Arithmatic Operation between (+,-,/,*) or q to quit: ").lower()
+        if user_operation == 'q':
+            quit()
+        else:
+            user_num1 = int(input("Enter fist NO: "))
+            user_num2 = int(input("Enter Second NO: "))
+            if user_operation == '+':
+                results = addition(a = user_num1,b = user_num2)
+                return f"Your Results: {results}"
+                
+            elif user_operation == '-':
+                results = substraction(a = user_num1,b = user_num2)
+                return f"Your Results: {results}"
+            elif user_operation == '*':
+                results = multiplication(a = user_num1,b = user_num2)
+                return f"Your Results: {results}"
+            elif user_operation == '/':
+                results = division(a = user_num1, b = user_num2)
+                return f"Your Results: {results}"
+            
+            else:
+                if user_num1 == 0 or user_num2 == 0:
+                    raise ValueError("Enter only numbers higher than 0")
+                else:
+                    print("choose correct operation ")
+        
+    def addition(a,b):
+        return a + b
+        
+    def substraction(a,b):
+        return a - b
+        
+    def multiplication(a,b):
+        return a * b
+        
+    def division(a,b):
+        return a/b
+
+    if __name__ == "__main__":
+        print(main())
