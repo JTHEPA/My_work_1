439042c2a0c3d72d5b47b079cff6e7de51822f22
joseph <mmthejhb025@student.wethinkcode.co.za>
2026-01-15 09:12:26 +0200

    3rd_done

--- basics/file_read.py
+++ basics/file_read.py
@@ -1,10 +1,13 @@
 count = 0
-with open("new.txt","r") as file:
-    file_words = file.read()
-    words = file_words.split()
-    count += len(words)
-    
+try:
+    with open("new.txt","r") as file:
+        file_words = file.read()
+        words = file_words.split()
+        count += len(words)
+except FileNotFoundError:
+    print("file not found")
 print(f"Number of words in file : {count}")
+
     
     
     
